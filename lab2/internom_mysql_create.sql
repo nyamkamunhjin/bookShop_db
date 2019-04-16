44ebbcbed0f710b5c6d938489bdd2141aede2be7
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2019-04-16 15:54:17 +0800

    update Tue April/16/19

--- lab2/internom_mysql_create.sql
+++ lab2/internom_mysql_create.sql
@@ -1,6 +1,9 @@
+drop database if exists internom;
+create database internom;
+use internom;
 
 CREATE TABLE `customers` (
-	`customer_id` varchar(5) NOT NULL,
+	`customer_id` int(3) NOT NULL AUTO_INCREMENT,
 	`last_name` varchar(30) NOT NULL,
 	`first_name` varchar(30) NOT NULL,
 	`gender` enum('male', 'female') NOT NULL,
@@ -10,7 +13,7 @@ CREATE TABLE `customers` (
 );
 
 CREATE TABLE `branch` (
-	`branch_id` varchar(5) NOT NULL,
+	`branch_id` int(3) NOT NULL AUTO_INCREMENT,
 	`branch_name` varchar(100) NOT NULL,
 	`city` varchar(30) NOT NULL,
 	`region` varchar(255) NOT NULL,
@@ -20,12 +23,13 @@ CREATE TABLE `branch` (
 );
 
 CREATE TABLE `product_branch` (
-	`product_id` varchar(5) NOT NULL,
-	`branch_id` varchar(5) NOT NULL
+	`product_id` int(3) NOT NULL AUTO_INCREMENT,
+	`branch_id` int(3) NOT NULL AUTO_INCREMENT,
+	`quantity` int(4) NOT NULL
 );
 
 CREATE TABLE `products` (
-	`product_id` varchar(5) NOT NULL,
+	`product_id` int(3) NOT NULL AUTO_INCREMENT,
 	`product_name` varchar(255) NOT NULL,
 	`author` varchar(255),
 	`description` varchar(255) NOT NULL,
@@ -34,42 +38,46 @@ CREATE TABLE `products` (
 	`product_type` varchar(50),
 	`price` int(10) NOT NULL,
 	`published_date` DATETIME NOT NULL,
-	`category_id` varchar(5) NOT NULL,
-	`publisher_id` varchar(5),
-	`width` int(4) NOT NULL,
+	`category_id` int(3) NOT NULL AUTO_INCREMENT,
+	`publisher_id` int(3),
+	`width AUTO_INCREMENT` int(4) NOT NULL,
 	`height` int(4) NOT NULL,
 	`depth` int(4) NOT NULL,
 	`total_page_num` int(4) NOT NULL,
 	`weight` int(4) NOT NULL,
 	`age_range_start` int(2),
 	`age_range_end` int(2),
+	`quantity` int(4) NOT NULL,
 	PRIMARY KEY (`product_id`)
 );
 
 
 CREATE TABLE `orders` (
-	`order_id` varchar(5) NOT NULL,
-	`product_id` varchar(5) NOT NULL,
+	`order_id` int(3) NOT NULL AUTO_INCREMENT,
+	`customer_id` int(3) NOT NULL AUTO_INCREMENT,
+	`product_id` int(3) NOT NULL AUTO_INCREMENT,
+	`branch_id` int(3) NOT NULL AUTO_INCREMENT,
 	`price` int(10) NOT NULL,
 	`quantity` int(4) NOT NULL,
-	`order_date` DATETIME NOT NULL,
-	`required_date` DATETIME NOT NULL,
+	`order_date` DATETIME,
+	`required_date` DATETIME,
 	`ship_address` varchar(255) NOT NULL,
 	`ship_city` varchar(50) NOT NULL,
 	`ship_region` varchar(50) NOT NULL,
 	`postal_code` varchar(50) NOT NULL,
+	`isConfirmed` tinyint(1),
 	PRIMARY KEY (`order_id`)
 );
 
 CREATE TABLE `shopping_cart` (
-	`cart_id` varchar(5) NOT NULL,
-	`customer_id` varchar(5) NOT NULL,
-	`order_id` varchar(5),
+	`cart_id` int(3) NOT NULL AUTO_INCREMENT,
+	`customer_id` int(3) NOT NULL AUTO_INCREMENT,
+	`order_id` int(3) NOT NULL AUTO_INCREMENT,
 	PRIMARY KEY (`cart_id`)
 );
 
 CREATE TABLE `category` (
-	`category_id` varchar(5) NOT NULL,
+	`category_id` int(3) NOT NULL AUTO_INCREMENT,
 	`name` varchar(25) NOT NULL,
 	`description` varchar(255) NOT NULL,
 	`featured` tinyint(1) NOT NULL,
@@ -77,13 +85,13 @@ CREATE TABLE `category` (
 );
 
 CREATE TABLE `publisher` (
-	`publisher_id` varchar(5) NOT NULL,
+	`publisher_id` int(3) NOT NULL AUTO_INCREMENT,
 	`publisher_name` varchar(25) NOT NULL,
 	PRIMARY KEY (`publisher_id`)
 );
 
 CREATE TABLE `blog` (
-	`blog_id` varchar(5) NOT NULL,
+	`blog_id` int(3) NOT NULL AUTO_INCREMENT,
 	`blog_name` varchar(10) NOT NULL,
 	`description` varchar(255) NOT NULL,
 	`date` DATETIME,
@@ -94,7 +102,11 @@ ALTER TABLE `products` ADD CONSTRAINT `products_fk0` FOREIGN KEY (`category_id`)
 
 ALTER TABLE `products` ADD CONSTRAINT `products_fk1` FOREIGN KEY (`publisher_id`) REFERENCES `publisher`(`publisher_id`);
 
-ALTER TABLE `orders` ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`);
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`);
+
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`branch_id`) REFERENCES `branch`(`branch_id`);
+
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk2` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`customer_id`);
 
 ALTER TABLE `shopping_cart` ADD CONSTRAINT `shopping_cart_fk0` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`customer_id`);
 
